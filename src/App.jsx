18006e5cbdbe6befe2e390b31768d4c97038d103
Eduardo G Nieva <edu@riddler.com.ar>
2022-02-05 22:08:03 -0300

    Agregué estilos para la integración del componente a la landing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css';
+import CartWidget from './components/CartWidget';
 import Navbar from './components/Navbar';
+
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <Navbar />
+      <CartWidget />
     </div>
   );
 }
