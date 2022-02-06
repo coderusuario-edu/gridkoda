1427d50c131c3994e3a4dfdfd3380862045161cc
Eduardo G Nieva <edu@riddler.com.ar>
2022-02-06 11:23:00 -0300

    Agregué el botón de Agregar al carrito al ItemCount

--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -15,11 +15,14 @@ const ItemCount = ({ stock, initial }) => {
         setMaximo(stock);
     }
     return (
-        <div className="btn-group" role="group" >
-            <button type="button" className="btn btn-bk" onClick={() => { if (cantidad > 1) { onSubstract() } }}>-</button>
-            <span className="btn btn-outline-bk"> {cantidad} </span>
-            <button type="button" className="btn btn-bk" onClick={() => { if (cantidad < stock) { onAdd() } }}>+</button>
-        </div>
+        <>
+            <div className="btn-group" role="group" >
+                <button type="button" className="btn btn-bk" onClick={() => { if (cantidad > 1) { onSubstract() } }}>-</button>
+                <span className="btn btn-outline-bk"> {cantidad} </span>
+                <button type="button" className="btn btn-bk" onClick={() => { if (cantidad < stock) { onAdd() } }}>+</button>
+            </div>
+            <button className="btn btn-outline-bk mt-1"> <small> Agregar al carrito</small></button>
+        </>
     );
 }
 
