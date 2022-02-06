3ba118e3d5adf701f743cddd6e84cea62fcf5e25
Eduardo G Nieva <edu@riddler.com.ar>
2022-02-05 22:12:06 -0300

    Agregué el componente ItemListContainer

---
+++ src/components/ItemListContainer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const ItemListContainer = ({greeting}) => {
+  return (<div>
+      <h2>{greeting}</h2>
+      <h3>tendida por sus propios dueños</h3>
+  </div>);
+};
+
+export default ItemListContainer;
