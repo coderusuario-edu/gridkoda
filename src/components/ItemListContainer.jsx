6a4ac80305e0275163d170bc4ba1a40b72537078
Eduardo G Nieva <edu@riddler.com.ar>
2022-02-06 11:17:38 -0300

    Ups... clase mal puesta..

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import tree1 from '../imgs/tree1.png'
+import ItemCount from './ItemCount';
 
 
 const ItemListContainer = ({greeting}) => {
@@ -8,12 +9,13 @@ const ItemListContainer = ({greeting}) => {
       <h3>tendida por sus propios dueños</h3>
       <div className="row mt-3">
           <div className="col-md-2 p-3">
-              <div className="card bg-bk mx-3">
+              <div className="card mx-3">
                 <img className="card-img-top p-3"src={tree1} alt=''/>
                 <div className="card-body">
                   <h4 className="card-title">Producto</h4>
                   <p className="card-text">Precio</p>
                 </div>
+                <ItemCount stock={5} initial={1}/>
               </div>
           </div>
       </div>
