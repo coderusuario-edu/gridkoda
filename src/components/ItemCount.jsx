ba88192d6369d7a33a1b229d9c39f8ce46c52d00
Eduardo G Nieva <edu@riddler.com.ar>
2022-02-13 22:40:37 -0300

    Indentacion

--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 
 const ItemCount = ({ stock, initial }) => {
-
     const [cantidad, setCantidad] = useState(initial);
     const [queda, setQueda] = useState(stock - 1);
 
@@ -10,42 +9,71 @@ const ItemCount = ({ stock, initial }) => {
             setCantidad(cantidad + 1);
             setQueda(queda - 1);
         }
-    }
+    };
 
     const substractItem = () => {
         if (cantidad > 1) {
             setCantidad(cantidad - 1);
-            setQueda(queda + 1)
+            setQueda(queda + 1);
         }
-
-    }
+    };
 
     const onAdd = () => {
         if (cantidad < stock) {
             alert(`Yay, agregaste al carrito ${cantidad} items`);
         }
-    }
+    };
     return (
         <>
-            <div className="btn-group" role="group" >
-                <button type="button" className="btn btn-bk" onClick={() => { substractItem() }}>-</button>
+            <div className="btn-group" role="group">
+                <button
+                    type="button"
+                    className="btn btn-bk"
+                    onClick={() => {
+                        substractItem();
+                    }}
+                >
+                    -
+                </button>
                 <span className="btn btn-outline-bk"> {cantidad} </span>
-                <button type="button" className={cantidad === stock ? 'btn btn-bk disabled' : 'btn btn-bk'} onClick={() => { addItem() }}>+</button>
+                <button
+                    type="button"
+                    className={
+                        cantidad === stock
+                            ? "btn btn-bk disabled"
+                            : "btn btn-bk"
+                    }
+                    onClick={() => {
+                        addItem();
+                    }}
+                >
+                    +
+                </button>
             </div>
-            {
-                queda > 0 ?
-                    (<>
-                        <small className="small">Quedan {queda} productos</small>
-                        <button className="btn btn-outline-bk mt-1" onClick={() => { onAdd() }} > <small> Agregar al carrito</small></button>
-                    </>)
-                    :
-                    (<>
-                        <small className="small">Sin stock</small>
-                        <button className="btn btn-outline-bk mt-1" disabled > <small> Agregar al carrito</small></button>
-                    </>)
-            }
+            {queda > 0 ? (
+                <>
+                    <small className="small">Quedan {queda} productos</small>
+                    <button
+                        className="btn btn-outline-bk mt-1"
+                        onClick={() => {
+                            onAdd();
+                        }}
+                    >
+                        {" "}
+                        <small> Agregar al carrito</small>
+                    </button>
+                </>
+            ) : (
+                <>
+                    <small className="small">Sin stock</small>
+                    <button className="btn btn-outline-bk mt-1" disabled>
+                        {" "}
+                        <small> Agregar al carrito</small>
+                    </button>
+                </>
+            )}
         </>
     );
-}
+};
 
 export default ItemCount;
