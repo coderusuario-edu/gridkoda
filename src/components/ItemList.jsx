138df01c6280fcc833888c2453afd9f279656ad2
Eduardo G Nieva <edu@riddler.com.ar>
2022-02-13 22:41:23 -0300

    Creación de componente y mapeo de la lista de productos

---
+++ src/components/ItemList.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Item from "./Item";
+
+const ItemList = ({ products }) => {
+    return (
+        <div className="row row-cols-1 row-cols-md-4 g-4">
+            {products &&
+                products.map((item) => (
+                    <Item
+                        key={item.id}
+                        nombre={item.title}
+                        descripcion={item.description}
+                        precio={item.price}
+                        imagen={item.pictureURL}
+                    />
+                ))}
+        </div>
+    );
+};
+
+export default ItemList;
