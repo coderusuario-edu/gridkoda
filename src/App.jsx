d853aa453f346ef87b5be79d9c2582029228b9ad
Eduardo G Nieva <edu@riddler.com.ar>
2022-02-16 00:24:59 -0300

    LLamada a ItemDetailContainer desde App para probar

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import './App.css';
-import CartWidget from './components/CartWidget';
-import ItemListContainer from './components/ItemListContainer';
-import Navbar from './components/Navbar';
-
+import "./App.css";
+import CartWidget from "./components/CartWidget";
+import ItemDetailContainer from "./components/ItemDetailContainer";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <ItemListContainer greeting="Bienvenidos a la tienda de Grid y Koda"/>
-      <CartWidget />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Navbar />
+            {/* <ItemListContainer greeting="Bienvenidos a la tienda de Grid y Koda"/> */}
+            <ItemDetailContainer itemPedido={2} />
+            <CartWidget />
+        </div>
+    );
 }
 
 export default App;
