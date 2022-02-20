f7a8bfb1e9fd5c31b79f13e76586dc3ea8a63828
Eduardo G Nieva <edu@riddler.com.ar>
2022-02-19 21:40:50 -0300

    Soporte para RRD desde la navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,33 +1,50 @@
-import React from 'react';
-import logo from '../logo.png';
+import React from "react";
+import logo from "../logo.png";
+import { NavLink } from "react-router-dom";
 
 function Navbar() {
-	return <div>
-		<nav className="navbar navbar-expand-lg navbar-light bg-gk-gradient">
-			<div className="container-fluid">
-				<a href="#" className="navbar-brand">
-					<img src={logo} alt="GridKoda" width="15%"/></a>
-				<button className="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav">
-					<span className="navbar-toggler-icon"></span>
-				</button>
-				<div className="collapse navbar-collapse justify-content-end" id="navbarNav">
-					<ul className="navbar-nav">
-						<li className="nav-item">
-							<a href="#" className="nav-link">Más comprados</a>
-						</li>
-						<li className="nav-item">
-							<a href="#" className="nav-link">Promociones</a>
-						</li>
-						<li className="nav-item">
-							<a href="#" className="nav-link">Contacto</a>
-						</li>
-					</ul>
-				</div>
-			</div>
-		</nav>
-
-
-	</div>;
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-light bg-gk-gradient">
+                <div className="container-fluid">
+                    <NavLink to="/" className="navbar-brand">
+                        <img src={logo} alt="GridKoda" width="15%" />
+                    </NavLink>
+                    <button
+                        className="navbar-toggler"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#navbarNav"
+                        aria-controls="navbarNav"
+                    >
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div
+                        className="collapse navbar-collapse justify-content-end"
+                        id="navbarNav"
+                    >
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <NavLink
+                                    to="/category/mas-comprados"
+                                    className="nav-link"
+                                >
+                                    Más comprados
+                                </NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink
+                                    to="/category/sale"
+                                    className="nav-link"
+                                >
+                                    Promociones
+                                </NavLink>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    );
 }
 
 export default Navbar;
