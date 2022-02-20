15d8cb42f2de72fe9a708ab3e30c0131e3d06794
Eduardo G Nieva <edu@riddler.com.ar>
2022-02-19 21:40:13 -0300

    Agregar soporte para RRD desde App

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,42 @@
 import "./App.css";
 import CartWidget from "./components/CartWidget";
+import ItemListContainer from "./components/ItemListContainer";
 import ItemDetailContainer from "./components/ItemDetailContainer";
 import Navbar from "./components/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
     return (
-        <div className="App">
-            <Navbar />
-            {/* <ItemListContainer greeting="Bienvenidos a la tienda de Grid y Koda"/> */}
-            <ItemDetailContainer itemPedido={2} />
-            <CartWidget />
-        </div>
+        <BrowserRouter>
+            <div className="App">
+                <Navbar />
+                <Routes>
+                    <Route
+                        path="/"
+                        element={
+                            <ItemListContainer
+                                greeting={
+                                    "Bienvenidos a la tienda de Grid y Koda"
+                                }
+                            />
+                        }
+                    />
+                    <Route
+                        path="/category/:catId"
+                        element={
+                            <ItemListContainer
+                                greeting={"Lista de productos por categoría"}
+                            />
+                        }
+                    />
+                    <Route
+                        path="/item/:itemId"
+                        element={<ItemDetailContainer />}
+                    />
+                </Routes>
+                <CartWidget />
+            </div>
+        </BrowserRouter>
     );
 }
 
