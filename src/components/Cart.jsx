4def7e7085ba6065a5e43600c3137c0dca3b6e6f
Eduardo G Nieva <edu@riddler.com.ar>
2022-02-21 17:24:04 -0300

    Cree el componente Carrito

---
+++ src/components/Cart.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Cart = () => {
+    return (
+        <div>
+            <h3>Carrito en construcción</h3>
+            <p>
+                nuestros trabajadores están doble horario para llegar al
+                Timeline por favor no los acaricie ni les traiga Katnip
+            </p>
+            <img
+                src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.imgflip.com%2F4%2Fg7u55.jpg&f=1&nofb=1"
+                alt=""
+            />
+        </div>
+    );
+};
+
+export default Cart;
