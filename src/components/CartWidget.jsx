326f3239362bea7c9e54f1124e42fa781fd59ec8
Eduardo G Nieva <edu@riddler.com.ar>
2022-02-28 16:53:27 -0300

    Agregar el contador dinámico al número de artículos del carrito

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,13 +1,21 @@
-import React from 'react';
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+import React from "react";
+import useCartContext from "../context/CartContext";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
 
 const CartWidget = () => {
-  return (<div className='carty'>
-    <span className='fontAwesomeCart'><FontAwesomeIcon icon={faShoppingCart} /></span>
-    <span className='superUp'>0</span>
-  </div>);
+    const { cart } = useCartContext();
+    return (
+        <Link to="/cart">
+            <div className="carty">
+                <span className="fontAwesomeCart">
+                    <FontAwesomeIcon icon={faShoppingCart} />
+                </span>
+                <span className="superUp">{cart.length}</span>
+            </div>
+        </Link>
+    );
 };
 
 export default CartWidget;
