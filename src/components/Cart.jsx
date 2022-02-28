d13f7b8b9c1733af91d01fc2e76470b3e70a10c9
Eduardo G Nieva <edu@riddler.com.ar>
2022-02-28 16:52:09 -0300

    contador dinámico en el CartWidget

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,6 +1,9 @@
 import React from "react";
+import useCartContext from "../context/CartContext";
 
 const Cart = () => {
+    const { cart } = useCartContext();
+
     return (
         <div>
             <h3>Carrito en construcción</h3>
@@ -12,6 +15,14 @@ const Cart = () => {
                 src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.imgflip.com%2F4%2Fg7u55.jpg&f=1&nofb=1"
                 alt=""
             />
+            <ul>
+                {cart.map((item, key) => (
+                    <li key={key}>
+                        id: {item.id}; producto: {item.title}; cantidad{" "}
+                        {item.quantity}
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 };
